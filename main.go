db76211bd9c3b1ae65165b27abd7470fbd9486e3
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-06-09 23:01:44 +0530

    Final finally complete

--- main.go
+++ main.go
@@ -3,14 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/Fakechippies/Virus-Total-API/attacktactics"
-	"github.com/Fakechippies/Virus-Total-API/domainreport"
-	"github.com/Fakechippies/Virus-Total-API/filebehaviour"
-	"github.com/Fakechippies/Virus-Total-API/ipreport"
 	"github.com/Fakechippies/Virus-Total-API/scanfile"
 	"github.com/Fakechippies/Virus-Total-API/scanurl"
-	"github.com/Fakechippies/Virus-Total-API/searchgraphs"
-	"github.com/Fakechippies/Virus-Total-API/threatcomparison"
 	"os"
 )
 
@@ -20,13 +14,7 @@ func main() {
 		fmt.Println("\nVirusTotal CLI")
 		fmt.Println("1. Scan a file for viruses")
 		fmt.Println("2. Scan a URL for malicious activity")
-		fmt.Println("3. Get IP address report")
-		fmt.Println("4. Get domain report")
-		fmt.Println("5. Get file behaviour")
-		fmt.Println("6. Get attack tactics")
-		fmt.Println("7. Popular threat comparison")
-		fmt.Println("8. Search graphs")
-		fmt.Println("9. Exit")
+		fmt.Println("3. Exit")
 		fmt.Print("Enter your choice: ")
 
 		scanner.Scan()
@@ -44,33 +32,6 @@ func main() {
 			url := scanner.Text()
 			scanurl.ScanURL(url)
 		case "3":
-			fmt.Print("Enter the IP address: ")
-			scanner.Scan()
-			ip := scanner.Text()
-			ipreport.GetIPReport(ip)
-		case "4":
-			fmt.Print("Enter the Domain: ")
-			scanner.Scan()
-			domain := scanner.Text()
-			domainreport.GetDomainReport(domain)
-		case "5":
-			fmt.Print("Enter the file path: ")
-			scanner.Scan()
-			filePath := scanner.Text()
-			filebehaviour.GetFileBehaviour(filePath)
-		case "6":
-			attacktactics.GetAttackTactics()
-		case "7":
-			fmt.Print("Enter the hash: ")
-			scanner.Scan()
-			hash := scanner.Text()
-			threatcomparison.PopularThreatComparison(hash)
-		case "8":
-			fmt.Print("Enter the hash(file, URL, domain, or IP): ")
-			scanner.Scan()
-			hash := scanner.Text()
-			searchgraphs.GetSearchGraph(hash)
-		case "9":
 			fmt.Println("Exiting...  ")
 			return
 		default:
