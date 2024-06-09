c2909fc6fe87493185dcb0e9ac109c0432209abd
Fakechippies <cherryprofessorpraveen@gmail.com>
2024-06-09 22:55:30 +0530

    Project is done

--- APIkeyScript.sh
+++ APIkeyScript.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Ascii art
+if ! command -v jp2a &> /dev/null; then
+    echo "jp2a is not installed. Installing jp2a..."
+    sudo apt update
+    sudo apt install -y jp2a
+fi
+jp2a --color-depth=24 "Gopher.png" 
+
 if [ ! -f .env ]; then
     read -p "Enter your VirusTotal API Key: " API_KEY
     echo "VIRUSTOTAL_API_KEY=$API_KEY" > .env
